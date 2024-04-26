45d0b988df4c7924f12055bb2b5393cecf9d6572
iLooToS <rhymudienullte@gmail.com>
2024-04-26 15:12:54 +0300

    merge-function

--- sudoku.js
+++ sudoku.js
@@ -23,29 +23,29 @@ const boardExample = [
 
 function read(num) {
   const fs = require('fs');
-  const { EOL } = require("os");
+  const { EOL } = require('os');
   const board = fs.readFileSync('puzzles.txt', 'utf-8');
   const boardAllSudoku = board.split(EOL);
   let arrSudoku;
   for (let i = 0; i <= boardAllSudoku.length; i += 1) {
-    arrSudoku = boardAllSudoku[num - 1].split("");
+    arrSudoku = boardAllSudoku[num - 1].split('');
     break;
   }
-  let playingField = [];
+  const playingField = [];
   for (let i = 0; i < arrSudoku.length; i += 9) {
     playingField.push(arrSudoku.slice(i, i + 9));
   }
   const result = playingField.map((row) => row.map((el) => {
     if (el === '-') {
-     return el = null;
-    } else {
-      return el
+      return el = null;
     }
-  }))
-  return playingField;
+    return Number(el);
+  }));
+  return result;
 }
 
-
+// console.table(read(1));
+const board = read(1);
 function solve(board) {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
@@ -59,11 +59,11 @@ function solve(board) {
       // console.table(board[i][j]);
       if (board[i][j] === null) {
         for (let k = 1; k < 10; k++) {
-          // if (true) {
-          if (isSolved(board, i, j, k)) {
+          if (true) {
+          // if (isSolved(board, i, j, k)) {
             board[i][j] = k;
             if (solve(board)) {
-              return true;
+              return board;
             }
             board[i][j] = null;
           }
@@ -72,82 +72,73 @@ function solve(board) {
       }
     }
   }
-  return true;
+  return board;
 }
 
-function isSolved(arr) {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
-  let result = true
-  let count = 1
+console.table(solve(board));
 
-  for(let i = 1; i <= arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      count++
-      if (arr[i][j] === arr[i+(10-count)][j] || arr[i][j] === arr[i+(9-count)][j] || arr[i][j] === arr[i+(8-count)][j] || arr[i][j] === arr[i+(7-count)][j] || arr[i][j] === arr[i+(6-count)][j] || arr[i][j] === arr[i+(5-count)][j] || arr[i][j] === arr[i+(4-count)][j] || arr[i][j] === arr[i+(3-counti)][j]) {
-        result = false
-      }
-      if (arr[i][j] === arr[i][j+(10-count)] || arr[i][j] === arr[i][j+(9-count)] || arr[i][j] === arr[i][j+(8-count)] || arr[i][j] === arr[i][j+(7-count)] || arr[i][j] === arr[i][j+(6-count)] || arr[i][j] === arr[i][j+(5-count)] || arr[i][j] === arr[i][j+(4-count)] || arr[i][j] === arr[i][j+(3-count)]) {
-        result = false
-      }
-      if (arr[i][j] === null) {
-        result = false
-      }
-      
-      // let cloneArr = [...arr]
-      // let newArr = []
-      // let oneArr = cloneArr.flat()
+// function isSolved(arr) {
+//   /**
+//    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//    * Возвращает булевое значение — решено это игровое поле или нет.
+//    */
+//   let result = true;
+//   let count = 1;
 
-      // let block1 = []
-      // let block2 = []
-      // let block3 = []
-      // let block4 = []
-      // let block5 = []
-      // let block6 = []
-      // let block7 = []
-      // let block8 = [] 
-      // let block9 = []
+//   for (let i = 1; i <= arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       count++;
+//       if (arr[i][j] === arr[i + (10 - count)][j] || arr[i][j] === arr[i + (9 - count)][j] || arr[i][j] === arr[i + (8 - count)][j] || arr[i][j] === arr[i + (7 - count)][j] || arr[i][j] === arr[i + (6 - count)][j] || arr[i][j] === arr[i + (5 - count)][j] || arr[i][j] === arr[i + (4 - count)][j] || arr[i][j] === arr[i + (3 - counti)][j]) {
+//         result = false;
+//       }
+//       if (arr[i][j] === arr[i][j + (10 - count)] || arr[i][j] === arr[i][j + (9 - count)] || arr[i][j] === arr[i][j + (8 - count)] || arr[i][j] === arr[i][j + (7 - count)] || arr[i][j] === arr[i][j + (6 - count)] || arr[i][j] === arr[i][j + (5 - count)] || arr[i][j] === arr[i][j + (4 - count)] || arr[i][j] === arr[i][j + (3 - count)]) {
+//         result = false;
+//       }
+//       if (arr[i][j] === null) {
+//         result = false;
+//       }
 
-      // for (let k = 0; k < oneArr.length; k++) {
-      //   (if k = 0 || k = 1)
-      // }
+//       // let cloneArr = [...arr]
+//       // let newArr = []
+//       // let oneArr = cloneArr.flat()
 
+//       // let block1 = []
+//       // let block2 = []
+//       // let block3 = []
+//       // let block4 = []
+//       // let block5 = []
+//       // let block6 = []
+//       // let block7 = []
+//       // let block8 = []
+//       // let block9 = []
 
+//       // for (let k = 0; k < oneArr.length; k++) {
+//       //   (if k = 0 || k = 1)
+//       // }
+//     }
+//   }
 
-    }
-  }
+//   return result;
+// }
 
-  return result
-}
-const fs = require('fs');
-const { EOL } = require('os');
-
-function prettyBoard(func) {
-  func = isSolved();
-  for (let i = 0; i < func.length; i++) {
-    if (i % 3 === 0 && i !== 0) {
-      console.log('--- --- ---');
-    }
-    let newStr = '';
-    for (let j = 0; j < func[i].length; j++) {
-      if (j % 3 === 0 && j !== 0) {
-        newStr += '|';
-      }
-      if (func[i][j] === 0) {
-        newwStr += ' ';
-      } else {
-        newwStr += func[i][j];
-      }
-      newStr += ' ';
-    }
-    console.log(newStr);
-  }
-}
-
-prettyBoard(isSolved());
-
-console.table(solve(boardExperemental));
-console.log(boardExperemental.flat().length);
-
+// function prettyBoard(board) {
+//   for (let i = 0; i < board.length; i++) {
+//     if (i % 3 === 0 && i !== 0) {
+//       console.log('--- --- ---');
+//     }
+//     let newStr = '';
+//     for (let j = 0; j < board[i].length; j++) {
+//       if (j % 3 === 0 && j !== 0) {
+//         newStr += '|';
+//       }
+//       if (board[i][j] === 0) {
+//         newStr += ' ';
+//       } else {
+//         newStr += board[i][j];
+//       }
+//       newStr += ' ';
+//     }
+//     console.log(newStr);
+//   }
+//   return newStr;
+// }
